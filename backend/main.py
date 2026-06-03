78ee47b9520bef9152d5bcf248e60cfba118e69a
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-06-03 17:36:50 +0530

    fixing CORS ERROR

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500" , "https://extraordinary-kangaroo-76250e.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
